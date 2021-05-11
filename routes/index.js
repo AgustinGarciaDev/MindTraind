0d0040edecb7926e3479683fed83587c7542ed5c
SantiSarmiento <santisarmiento54@gmail.com>
2021-05-11 16:51:20 -0300

    controaladore user y course checkeados y funcionando

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const validator = require('../config/validator') */
 
 const courseControllers = require('../controllers/courseControllers')
+const userControllers = require('../controllers/userControllers')
 
 const {addCourse, getAllCourses, getCourseById, updateCourse, deleteCourse} = courseControllers
+const {addUser, getAllUsers, getUserById, updateUser, deleteUser} = userControllers
 
 //COURSE ROUTES
 router.route('/courses')
@@ -18,5 +20,13 @@ router.route('/courses/:id')
     .delete(deleteCourse)
 
 //USER ROUTES
+router.route('/users')
+    .post(addUser)
+    .get(getAllUsers)
 
+router.route('/users/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+    
 module.exports = router
