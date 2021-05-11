4a0fc9d3b50d4358138351e220f9605b447ac578
valentingracia <valentin.gracia@gmail.com>
2021-05-11 10:49:57 -0400

    misCambios

---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div>
+<form _ngcontent-qgr-c7="" novalidate="" class="mobile ng-pristine ng-invalid ng-touched"><!----><input _ngcontent-qgr-c7="" class="form-control form-control-md ng-pristine ng-invalid ng-touched" formcontrolname="user" type="text" id="user" placeholder="RUT"><!----><div _ngcontent-qgr-c7="" appinputtooltip="" style="position: relative;"><input _ngcontent-qgr-c7="" class="form-control form-control-md ng-pristine ng-invalid ng-touched" formcontrolname="password" type="password" id="password" placeholder="Clave Internet"><!----></div><!----><button _ngcontent-qgr-c7="" type="submit" class="btn btn-primary" disabled=""> Ingresar </button></form>
+        </div>
+    )
+}
+
+export default SignUp
