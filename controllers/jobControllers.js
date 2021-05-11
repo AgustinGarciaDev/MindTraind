c19a20220f571ca52b81c615f83d4978e8258199
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2021-05-11 16:54:59 -0300

    se agregaron los controladores de Job

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,1 +1,76 @@
 const Job = require('../models/Jobs');
+
+
+const respondFrontend = (res, response, error) => {
+    res.json({
+        success: !error ? true : false,
+        response,
+        error
+    })
+}
+const errorBackend = "error 500 , avisar al  team backend";
+const errorJobNotFound = "error: Job not found";
+
+const jobControllers = {
+    addJob = async (req,res) => {
+        let respuesta, error;
+        try {
+            respuesta = new Job(req.body);
+            await respuesta.save();
+        } catch (err) {
+            console.log(err);
+            error = errorBackend;
+        }
+        respondFrontend(req,res);
+    },
+    getAllJobs = async (req,res) => {
+        let respuesta, error;
+        try {
+            respuesta = await Job.find();
+        } catch (err) {
+            console.log(err);
+            error = errorBackend;
+        }
+        respondFrontend(req,res);
+    },
+    getJobById = async (req,res) => {
+        const id = req.params.id;
+        let respuesta, error;
+        try {
+            respuesta = await Job.findById(id);
+            respuesta || (error = errorJobNotFound)
+        } catch (err) {
+            console.log(err);
+            error = errorBackend;
+        }
+        respondFrontend(req,res);
+    },
+    updateJob: async (req,res) => {
+        const id = req.params.id;
+        let respuesta, error;
+        try {
+            respuesta = await Job.findByIdAndUpdate(id,req.body,{new:true});
+            respuesta || (error = errorJobNotFound);
+        } catch (err) {
+            console.log(err);
+            error = errorBackend;
+        }
+        respondFrontend(req,res);
+    },
+    deleteJob: async (req,res) => {
+        const id = req.params.id;
+        let respuesta, error;
+        try {
+            respuesta = await Job.findByIdAndDelete(id);
+            respuesta || (error = errorJobNotFound);
+        } catch (err) {
+            console.log(err);
+            error = errorBackend;
+        }
+        respondFrontend(req,res);
+    }
+
+}
+
+module.exports = jobControllers;
+
