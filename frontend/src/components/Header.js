02acabfa9b06903e59e0199deb3e031e5d373e37
camila <camiladomato@gmail.com>
2021-05-11 15:31:54 -0300

    arreglo en header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,37 @@ import React from 'react'
 import{NavLink} from 'react-router-dom'
 
 const Header=()=>{
-    return(
-    <div>   
-        <div  className="header">
-            <div className="link-footer">
-                <NavLink exact to="/"><h2 className="link">Home</h2></NavLink>
-                <NavLink exact to="/signup"><h2 className="link">signup</h2></NavLink>
-                <NavLink exact to="/signin"><h2 className="link">signin</h2></NavLink>
-                <NavLink exact to="/dashboard"><h2 className="link">dashboard</h2></NavLink>
-            </div>
-       </div>     
-</div>
-    )
+  return (
+    <header>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Logo</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#deets">More deets</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                        Dank memes
+  </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </header>
+)
 }
+
+    
+
+           
+
 export default Header
