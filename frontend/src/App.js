fa490a4d518226234ff4b41c1810abc2349abb64
Agustin Garcia <agustinchinchilla16@gmail.com>
2021-05-11 11:45:36 -0300

    Creando rutas en el archivo app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
-function App() {
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+
+const App = () => {
+
   return (
-
-    <h1>Hello Word!</h1>
-
-  );
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
