203e4db65f891b69ca1ffc769c2fb4aa7c1c317a
Agustin Garcia <agustinchinchilla16@gmail.com>
2021-05-11 13:36:28 -0300

    Arreglando problema de rutas archivo app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ const App = () => {
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
       <Footer />
+
     </BrowserRouter>
   )
 }
--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,13 +1,13 @@
 const Dashboard = () => {
 
     const cursos = [
-        { nombre: "zumba", foto: "http://baravdg.com/wp-content/uploads/2021/05/d8.jpg", nombre: "profesor", fecha: "17-05-2022", duration: "3 meses", dificultad: "extrema" }
+        { nombreCurso: "zumba", foto: "http://baravdg.com/wp-content/uploads/2021/05/d8.jpg", nombre: "profesor", fecha: "17-05-2022", duration: "3 meses", dificultad: "extrema" }
     ]
 
     return (
         <div>
             {cursos.map(curso => <div>
-                <h1>{curso.nombre}</h1>
+                <h1>{curso.nombreCurso}</h1>
             </div>)}
         </div>
     )
