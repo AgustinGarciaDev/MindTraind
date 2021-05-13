bd865a4ac0cbb5bc20503812dc2389d71b31a057
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2021-05-13 18:24:19 -0300

    se agrego comments al modelo Course y urlImage al modelo Job

--- models/Course.js
+++ models/Course.js
@@ -3,17 +3,16 @@ const mongoose = require('mongoose')
 const courseSchemma = new mongoose.Schema({
     nameCourse: { type: String, required: true },
     categories: [{ name: { type: String, required: true } }],
-    /*     startingDate: [{ type: Date }], */
     coach: { type: mongoose.Types.ObjectId, ref: 'user' },
     pictureRefence: { type: String, required: true },
     programDescription: { type: String, required: true },
     lessons: [{ lessonName: { type: String, required: true }, videoLink: { type: String, required: true } }],
     duration: { type: Number, required: true },
     difficulty: { type: Number, required: true, min: 1 },
-    students: { type: [{ type: mongoose.Types.ObjectId, ref: 'user' }], default: [] }
-    //[{type: mongoose.Types.ObjectId,ref: 'user'}]
+    students: { type: [{ type: mongoose.Types.ObjectId, ref: 'user' }], default: [] },
+    comments: [{user:{type: mongoose.Types.ObjectId, ref:'user'},text:{type:String} }]
 })
-
+//comentarios :[{usuarioId:{type: mongoose.Types.ObjectId, ref: 'user'},comentario:{type:String} }],
 const Course = mongoose.model('course', courseSchemma)
 
 module.exports = Course
