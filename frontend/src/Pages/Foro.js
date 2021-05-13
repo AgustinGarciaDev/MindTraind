20511286c7c0870fd0e93af4a71724812ef5ff2f
Agustin Garcia <agustinchinchilla16@gmail.com>
2021-05-13 11:20:21 -0300

    Agregando estilos en foro

--- frontend/src/Pages/Foro.js
+++ frontend/src/Pages/Foro.js
@@ -1,80 +1,58 @@
 import NavBarDashBoard from '../components/NavBarDashBoard'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import Post from '../components/Post'
-import { convertFromRaw, convertToRaw } from 'draft-js';
 import { useEffect, useState } from "react";
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
 
-/* import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
- */
+
 const Foro = () => {
     const [modalShow, setModalShow] = useState(false);
     const nameUser = "Agustin"
     const post = [
-        { name: "agustin", apellido: "garcia", comment: "commentario nuevo" },
-        { name: "agustin", apellido: "garcia", comment: "commentario nuevo" },
-        { name: "agustin", apellido: "garcia", comment: "commentario nuevo" },
-        { name: "agustin", apellido: "garcia", comment: "commentario nuevo" },
+        { name: "agustin", apellido: "garcia", comment: "commentario nuevo", titulo: "Consulta 1", foto: "http://baravdg.com/wp-content/uploads/2021/04/46.jpg" },
+        { name: "agustin", apellido: "garcia", comment: "commentario nuevo", titulo: "Consulta 1", foto: "http://baravdg.com/wp-content/uploads/2021/04/46.jpg" },
+        { name: "agustin", apellido: "garcia", comment: "commentario nuevo", titulo: "Consulta 1", foto: "http://baravdg.com/wp-content/uploads/2021/04/46.jpg" },
+        { name: "agustin", apellido: "garcia", comment: "commentario nuevo", titulo: "Consulta 1", foto: "http://baravdg.com/wp-content/uploads/2021/04/46.jpg" },
     ]
 
 
-    const [editorState, setEditorState] = useState(EditorState.createEmpty())
 
-    const onEditorStateChange = (editorState) => {
 
-        setEditorState(editorState)
-    }
-
-    const info = convertToRaw(editorState.getCurrentContent())
-    console.log(info)
-    const rawState = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
-
-    console.log(rawState)
-
-    /*     const conver = draftToHtml(convertToRaw(editorState.getCurrentContent()))
-    
-        console.log(conver) */
     return (
         <>
             <NavBarDashBoard />
             <main className="contenedorPosteos">
+                <div className="barraBuscadora">
+                    <input placeholder="Search Post" type="text" />
+                    <i class="fas fa-search"></i>
+                </div>
                 <div className="contenedorBtnTextArea">
 
                     <div onClick={() => { setModalShow(!modalShow) }} className="contenedorBienvenidaUsuario">
                         <img className="logoDashBoard" src="http://baravdg.com/wp-content/uploads/2021/04/46.jpg" alt="" />
-                        <h4>
+                        <h4 className="tituloForm">
                             Hi {nameUser} Queres compartir algo?
                         </h4>
+                    </div>
+                    <div>
                     </div>
                     {!modalShow &&
                         <div className="postYtitulo">
                             <div>
-                                <h2>titulo</h2>
+                                <h2 className="titleInternalForm">titulo</h2>
                                 <input className="inputPost" type="text" />
                             </div>
                             <div>
-                                <h1>Content</h1>
-                                <div className="contenedorTextArea">
-                                    < Editor
-                                        editorState={editorState}
-                                        toolbarClassName="toolbarClassName"
-                                        wrapperClassName="wrapperClassName"
-                                        editorClassName="editorClassName"
-                                        onEditorStateChange={onEditorStateChange}
-                                    />
-                                </div>
+                                <h2 className="titleInternalForm" >Content</h2>
+                                <textarea className="textAreaConsulta" name="" id="" cols="30" rows="10"></textarea>
                             </div>
+                            <div className="contenedorBtn">
+                                <button className="btnDashBoard btnForm">Send</button>
+                            </div>
+
                         </div>
                     }
                 </div>
-                <div>
-                    <div className="barraBuscadora">
-                        <input type="text" />
-                        <i class="fas fa-search"></i>
-                    </div>
-
+                <div className="contenedorComentarios">
                     {post.map(post => <Post post={post} />)}
                 </div>
 
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,8 +1,23 @@
 
 const Post = (props) => {
 
+    console.log(props)
+
+    const { apellido, name, titulo, comment, foto } = props.post
+
+    console.log(titulo)
+
     return (
-        <h1>Yo soy el posteo pa</h1>
+        <div className="contenedorPost">
+            <h2>{titulo}</h2>
+            <div>
+                <img className="comentarioFotoUser" src={foto} alt="" />
+                <div>
+                    <h3>{name}</h3>
+                    <h3>{apellido}</h3>
+                </div>
+            </div>
+        </div>
     )
 }
 
