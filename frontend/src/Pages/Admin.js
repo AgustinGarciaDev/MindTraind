a45ae7e50ed7f08b672c31546ffa22490182b1c0
SantiSarmiento <santisarmiento54@gmail.com>
2021-05-14 11:04:06 -0300

    ignorar este commit

--- frontend/src/Pages/Admin.js
+++ frontend/src/Pages/Admin.js
@@ -6,6 +6,8 @@ import coursesActions from "../redux/actions/coursesActtions"
 import { showToast } from '../helpers/myToast'
 import CategoryText from "../components/CategoryText"
 import LessonText from "../components/LessonText"
+import AsideNav from "../components/AsideNav"
+import Header from '../components/Header'
 
 const Admin = (props) => {
     const [loader, setLoader] = useState(true)
@@ -108,7 +110,7 @@ const Admin = (props) => {
 
     const changeLesson = (data) => {
         course.lessons.map(lesson => {
-            if(lesson.lessonName === data.id){
+            if (lesson.lessonName === data.id) {
                 lesson.lessonName = data.lessonName
                 lesson.videoLink = data.videoLink
                 return lesson
@@ -126,70 +128,76 @@ const Admin = (props) => {
 
     return (
         <>
-            <div className="adminContainer">
-                <h3 className="h3Form">Courses</h3>
-                <div className="courseContainer">
-                    {
-                        loader
-                            ?
-                            <Spinner animation="border" role="status" />
-                            :
-                            props.coursesList.map(course => <Course key={course._id} course={course} />)
-                    }
-                </div>
-                <div className="newCourseContainer">
-                    <form className="newCourseForm">
-                        <h3 className="h3Form">Add new course</h3>
-
-                        <input type="text" placeholder="Course name" name="nameCourse" value={course.nameCourse} onChange={readInput} />
-                        {error.nameCourse && <small>{error.nameCourse}</small>}
-
-                        <input type="text" placeholder="Program description" name="programDescription" value={course.programDescription} onChange={readInput} />
-                        {error.programDescription && <small>{error.programDescription}</small>}
-
-                        <input type="text" placeholder="Coach " name="coach" value={course.coach} onChange={readInput} />
-                        {error.coach && <small>{error.coach}</small>}
-
-                        <input type="text" placeholder="Picture refence " name="pictureRefence" value={course.pictureRefence} onChange={readInput} />
-                        {error.pictureRefence && <small>{error.pictureRefence}</small>}
-
-                        <input type="number" placeholder="Duration" name="duration" value={course.duration} onChange={readInput} />
-                        {error.duration && <small>{error.duration}</small>}
-
-                        <input type="number" placeholder="Difficulty" name="difficulty" value={course.difficulty} onChange={readInput} />
-                        {error.difficulty && <small>{error.difficulty}</small>}
-
-                        <h3 className="h3Form">Categories</h3>
-                        <div className="categoryNew">
-                            <div className="lessonInputError">
-                                <input type="text" placeholder="categories" onChange={createCategory} name="name" value={category.name} />
-                                {error.categories && <small>{error.categories}</small>}
-                            </div>
-                            <i className="fas fa-plus" onClick={addCategory}></i>
-                        </div>
-                        <div className="newCategories">
+            <div className="contenedorMenu">
+                <AsideNav />
+                <div className="contenedorWeb">
+                    <Header />
+                    <div className="adminContainer">
+                        <h3 className="h3Form">Courses</h3>
+                        <div className="courseContainer">
                             {
-                                course.categories.map(category => <CategoryText deleteCategory={deleteCategory} changeCategory={changeCategory} key={category.name} category={category} />)
+                                loader
+                                    ?
+                                    <Spinner animation="border" role="status" />
+                                    :
+                                    props.coursesList.map(course => <Course key={course._id} course={course} />)
                             }
                         </div>
-                        <h3 className="h3Form">Lessons</h3>
-                        <div className="lessonsNew">
-                            <div className="lessonInput">
-                                <div className="lessonInputError">
-                                    <input type="text" placeholder="lesson name" onChange={createLesson} name="lessonName" value={lesson.lessonName} />
-                                    <input type="text" placeholder="video" onChange={createLesson} name="videoLink" value={lesson.videoLink} />
-                                    {error.lessons && <small>{error.lessons}</small>}
+                        <div className="newCourseContainer">
+                            <form className="newCourseForm">
+                                <h3 className="h3Form">Add new course</h3>
+
+                                <input type="text" placeholder="Course name" name="nameCourse" value={course.nameCourse} onChange={readInput} />
+                                {error.nameCourse && <small>{error.nameCourse}</small>}
+
+                                <input type="text" placeholder="Program description" name="programDescription" value={course.programDescription} onChange={readInput} />
+                                {error.programDescription && <small>{error.programDescription}</small>}
+
+                                <input type="text" placeholder="Coach " name="coach" value={course.coach} onChange={readInput} />
+                                {error.coach && <small>{error.coach}</small>}
+
+                                <input type="text" placeholder="Picture refence " name="pictureRefence" value={course.pictureRefence} onChange={readInput} />
+                                {error.pictureRefence && <small>{error.pictureRefence}</small>}
+
+                                <input type="number" placeholder="Duration" name="duration" value={course.duration} onChange={readInput} />
+                                {error.duration && <small>{error.duration}</small>}
+
+                                <input type="number" placeholder="Difficulty" name="difficulty" value={course.difficulty} onChange={readInput} />
+                                {error.difficulty && <small>{error.difficulty}</small>}
+
+                                <h3 className="h3Form">Categories</h3>
+                                <div className="categoryNew">
+                                    <div className="lessonInputError">
+                                        <input type="text" placeholder="categories" onChange={createCategory} name="name" value={category.name} />
+                                        {error.categories && <small>{error.categories}</small>}
+                                    </div>
+                                    <i className="fas fa-plus" onClick={addCategory}></i>
                                 </div>
-                            </div>
-                            <i className="fas fa-plus" onClick={addLesson}></i>
+                                <div className="newCategories">
+                                    {
+                                        course.categories.map(category => <CategoryText deleteCategory={deleteCategory} changeCategory={changeCategory} key={category.name} category={category} />)
+                                    }
+                                </div>
+                                <h3 className="h3Form">Lessons</h3>
+                                <div className="lessonsNew">
+                                    <div className="lessonInput">
+                                        <div className="lessonInputError">
+                                            <input type="text" placeholder="lesson name" onChange={createLesson} name="lessonName" value={lesson.lessonName} />
+                                            <input type="text" placeholder="video" onChange={createLesson} name="videoLink" value={lesson.videoLink} />
+                                            {error.lessons && <small>{error.lessons}</small>}
+                                        </div>
+                                    </div>
+                                    <i className="fas fa-plus" onClick={addLesson}></i>
+                                </div>
+                                <div className="newCategories">
+                                    {
+                                        course.lessons.map(lesson => <LessonText changeLesson={changeLesson} key={lesson.lessonName} lesson={lesson} deleteLesson={deleteLesson} />)
+                                    }
+                                </div>
+                                <button className="formButtonsNew" onClick={sendData}>Add</button>
+                            </form>
                         </div>
-                        <div className="newCategories">
-                            {
-                                course.lessons.map(lesson => <LessonText changeLesson={changeLesson} key={lesson.lessonName} lesson={lesson} deleteLesson={deleteLesson} />)
-                            }
-                        </div>
-                        <button className="formButtonsNew" onClick={sendData}>Add</button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </>
--- frontend/src/components/AsideNav.js
+++ frontend/src/components/AsideNav.js
@@ -18,38 +18,38 @@ const AsideNav = () => {
                         <div className="listIconNav">
                             <div onClick={changeStatus} className="contenedorIconNav contenedorIconNavActive"><i class="fas fa-align-left"></i> </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fas fa-graduation-cap"></i><p> Courses</p>
+                                <i className="fas fa-graduation-cap"></i><p> Courses</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fas fa-border-all"></i><p> Dashboard</p>
+                                <i className="fas fa-border-all"></i><p> Dashboard</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fas fa-briefcase"></i><p>Jobs</p>
+                                <i className="fas fa-briefcase"></i><p>Jobs</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fab fa-discord"></i><p> Discord</p>
+                                <i className="fab fa-discord"></i><p> Discord</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fab fa-spotify"></i><p>Spotify</p>
+                                <i className="fab fa-spotify"></i><p>Spotify</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fas fa-users-cog"></i><p> Admin</p>
+                                <i className="fas fa-users-cog"></i><p> Admin</p>
                             </div>
                             <div className="contenedorIconNav contenedorIconNavActive">
-                                <i class="fas fa-door-open"></i><p> Sign out</p>
+                                <i className="fas fa-door-open"></i><p> Sign out</p>
                             </div>
                         </div>
                     </div>
                     : <div className="asideNav">
                         <div className="listIconNav">
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fas fa-align-left"></i> </div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fas fa-briefcase"></i> </div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fas fa-graduation-cap"></i></div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fas fa-border-all"></i></div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fab fa-discord"></i></div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fab fa-spotify"></i></div>
-                            <div onClick={changeStatus} className="contenedorIconNav"><i class="fas fa-users-cog"></i></div>
-                            <div onClick={changeStatus} className="contenedorIconNav"> <i class="fas fa-door-open"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fas fa-align-left"></i> </div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fas fa-briefcase"></i> </div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fas fa-graduation-cap"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fas fa-border-all"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fab fa-discord"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fab fa-spotify"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"><i className="fas fa-users-cog"></i></div>
+                            <div onClick={changeStatus} className="contenedorIconNav"> <i className="fas fa-door-open"></i></div>
                         </div>
                     </div>
             }
