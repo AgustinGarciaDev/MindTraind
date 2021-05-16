5136534a9d2565984f5fe1d5e7fe23a867b2348b
Agustin Garcia <agustinchinchilla16@gmail.com>
2021-05-16 16:18:28 -0300

    Agregando cambios en navBar

--- frontend/src/components/NavBarDashBoard.js
+++ frontend/src/components/NavBarDashBoard.js
@@ -1,26 +1,56 @@
 import { Nav } from "react-bootstrap";
 import { Navbar } from 'react-bootstrap';
 import { NavDropdown } from 'react-bootstrap';
+import usersActions from "../redux/actions/usersActions"
+import { connect } from "react-redux";
 
-const NavBarDashBoard = () => {
+const NavBarDashBoard = (props) => {
+
+    const desloguear = () => {
+        props.logOutUser()
+
+    }
+
 
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home"><img className="logoDashBoard" src="http://baravdg.com/wp-content/uploads/2021/05/tm.png" alt="" /></Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img className="logoDashBoard" src="http://baravdg.com/wp-content/uploads/2021/05/tm.png" alt="" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#pricing"><img className="DashBoardfotoUsuario" src="http://baravdg.com/wp-content/uploads/2021/04/46.jpg" alt="" /></Nav.Link>
+                        <Nav.Link href="#pricing">
+                            <div className="fotoProfesor" style={{ backgroundImage: `url("${props.userLogged.profilePicture}")` }}></div>
+                        </Nav.Link>
                         <NavDropdown title="Settings" id="collasible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Perfil</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Beneficios</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
+                    <Nav.Link href="#pricing">
+                        <p onClick={desloguear} className="logout">SIGN OUT</p>
+                    </Nav.Link>
+
                 </Navbar.Collapse>
             </Navbar>
         </header>
     )
 }
 
-export default NavBarDashBoard
+const mapStateToProps = state => {
+    return {
+        userLogged: state.user.userLogged,
+
+    }
+}
+
+
+const mapDispatchToProps = {
+
+    logOutUser: usersActions.logOutUser
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBarDashBoard)
